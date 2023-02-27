9c9bb40ed99dd6be4140bb3c54a5c60231acfc37
Andy Miller <andymill@redhat.com>
2023-02-27 14:25:47 -0500

    putting things where they should have been the whole time

--- src/main/java/me/crashloopbackoff/message/MessageApplication.java
+++ src/main/java/me/crashloopbackoff/message/MessageApplication.java
@@ -2,8 +2,10 @@ package me.crashloopbackoff.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MessageApplication {
 
 	public static void main(String[] args) {
--- src/main/java/me/crashloopbackoff/message/MessageController.java
+++ src/main/java/me/crashloopbackoff/message/MessageController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ConfigurationPropertiesScan
 public class MessageController {
 
-    final
+    private final
     MessageConfig config;
 
     public MessageController(MessageConfig config) {
